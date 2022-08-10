ade7770e44ba0e2670b8ae154f35395509370274
Ichtyander <di_ignatov@mail.ru>
2022-08-10 12:49:57 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Linear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    struct Linear
+    {
+        double k;
+        double b;
+
+        public Linear(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public double Root()
+        {
+            if (k == 0)
+            {
+                return double.NaN;
+            }
+            else
+            {
+                return -b / k;
+            }
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    /*1.   Разработать структуру для решения линейного уравнения 0=kx+b.
+     * Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа.
+     * Для решения уравнения предусмотреть метод Root.
+     * Создать экземпляр разработанной структуры.
+     * Осуществить использование экземпляра в программе.*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вычисление корня линейного уравнения kx+b=0");
+            Console.WriteLine();
+            Console.Write("Введите значение коэффициента k - ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение члена b - ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Linear linear = new Linear(k, b);
+            double root = linear.Root();
+
+            Console.WriteLine($"Значение корня уравнения - {root}");
+            Console.ReadKey();
+        }
+    }
+}
